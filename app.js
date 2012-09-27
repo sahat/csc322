8dc3764563d527c2ad80d0e66bc325aff2ce8a8d
Sahat Yalkabov <sakhat@gmail.com>
2012-09-26 22:07:54 -0400

    changed site template

--- app.js
+++ app.js
@@ -14,8 +14,7 @@
  * Module dependencies.
  */
 var express = require('express'),
-    routes = require('./routes'),
-    user = require('./routes/user'),
+    //hash = require('./pass').hash;
     http = require('http'),
     path = require('path');
 
@@ -24,7 +23,7 @@ var app = express();
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('view engine', 'ejs');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
@@ -37,11 +36,16 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/games', index);
-app.get('/games/:id', routes.index);
-app.get('/users', user.list);
-app.get('/users/:username', index);
+app.get('/', function(req, res) {
+  res.render('index', { title: 'ORANGECUBE' });
+});
+app.get('/login', function(req, res) {
+  res.render('login');
+});
+//app.get('/games', index);
+//app.get('/games/:id', routes.index);
+//app.get('/users', user.list);
+//app.get('/users/:username', index);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
