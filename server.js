b7100d0fee7a03ab00d414a20e3ddb6ac985d8ca
Sahat Yalkabov <sakhat@gmail.com>
2012-10-21 20:53:59 -0400

    added comment documentation

--- server.js
+++ server.js
@@ -1,13 +1,5 @@
-// kick for ratings -> 2 suspected spammer warnings (3 flags),
-// 1 flag per session at most
-// after 3 flags, suspectedCounter = 1, admin can ignore or
-// leave it suspended for further rating
-// kick 2 bad comment warnings
-
-
-// Status: OK - Everything is 0
-// Suspended: Rating Warnings is 3
-// Disabled: Comment Warnings is 2
+// By Sahat Yalkabov & Co.
+// CL4P-TP Online Store and Recommendation Engine
 
 var fs = require('fs');
 var express = require('express');
@@ -139,8 +131,6 @@ User.methods.comparePassword = function(candidatePassword, callback) {
   });
 };
 
-
-
 // After we create a schema, the next step is to create a model based on that schema.
 // A model is a class with which we construct documents.
 // In this case, each document will be a user with properties and behaviors as declared in our schema.
@@ -185,16 +175,22 @@ app.configure('development', function() {
  */
 
 app.get('/add', function (req, res) {
+
+  // only admin should be able to view Add Game page
   if (!req.session.user || req.session.user.isAdmin == false) {
     res.redirect('/');
   }
+
   res.render('add');
 });
 
 app.post('/add', function (req, res) {
-  var url = req.body.gameURL;
-  request({uri: url}, function (err, response, body) {
+
+  // create a client request to amazon.com
+  request({ uri: req.body.gameURL }, function (err, response, body) {
     if (err && response.statusCode !== 200) return;
+
+    // using jQuery to extract DOM information from Amazon.com
     jsdom.env({ html: body, scripts: ['http://code.jquery.com/jquery-1.6.min.js'] }, function (err, window) {
       var $ = window.jQuery;
 
@@ -206,10 +202,11 @@ app.post('/add', function (req, res) {
       var price = $('#listPriceValue').html() || $('#actualPriceValue').text() ;
       console.log(price);
 
+      // helper function to create a slug (e.g. Mass Effect 2 --> mass-effect-2)
       function slugify(text) {
         text = text.replace(/[^-a-zA-Z0-9\s]+/ig, '');
-        text = text.replace(/-/gi, "_");
-        text = text.replace(/\s/gi, "-");
+        text = text.replace(/-/gi, '_');
+        text = text.replace(/\s/gi, '-');
         text = text.toLowerCase();
         return text;
       }
@@ -220,20 +217,22 @@ app.post('/add', function (req, res) {
 
       // long game summary with screenshots
       if (!$('.productDescriptionWrapper .aplus').html()) {
-        console.log('aplus is not here');
+        console.log('no aplus class');
         var summary = $('.productDescriptionWrapper').html();
       }
+      // depending on the game, DOM might be structured differently on Amazon.com
       else {
-        console.log('using aplus');
+        console.log('using aplus class');
         var summary = $('.productDescriptionWrapper .aplus').html();
       }
 
+      // creating a new client request to Gamespot.com using sluggified title from Amazon.com
       request({uri: 'http://www.gamespot.com/' + slug + '/platform/pc/'}, function (err, response, body) {
         if (err && response.statusCode !== 200) return;
         jsdom.env({ html: body, scripts: ['http://code.jquery.com/jquery-1.6.min.js'] }, function (err, window) {
           var $ = window.jQuery;
 
-          // game description
+          // short description
           var description = $('.productDeck .mainDeck').text();
           console.log(description);
 
@@ -241,7 +240,7 @@ app.post('/add', function (req, res) {
           var genre = $('.genre .data').text();
           console.log(genre);
 
-          // game publisher
+          // publisher
           var publisher = $('.publisher .data').text();
           console.log(publisher);
 
@@ -257,7 +256,6 @@ app.post('/add', function (req, res) {
           var thumbnail = slug + '-thumb.jpg';
           console.log(thumbnail);
 
-
           // large cover for the game
           // I used a regular expression 'replace' to replace thumb with front to match valid Gamespot URL
           var tempLarge = $('.boxshot a img').attr('src');
@@ -266,7 +264,7 @@ app.post('/add', function (req, res) {
           console.log(largeImage);
           var largeImage = slug + '-large.jpg';
 
-          // save all above data to MongoDB
+          // game is a Schema object containing parsed information
           var game = new Game({
             title: title,
             slug: slug,
@@ -280,13 +278,19 @@ app.post('/add', function (req, res) {
             releaseDate: releaseDate,
           });
 
+          // Prevents adding the same game twice
           Game.findOne({ 'slug': slug }, function (err, game) {
             if (game)
               res.send(500, 'Halt: Games already exists');
           });
+
+          // Save game object into database as a document of db.games collection
           game.save(function(err) {
-            if (err) throw err;
-            console.log('saved game into db');
+            if (err) {
+              console.log(err);
+              res.send(500, 'Unable to add the game to database');
+            }
+            console.log('Added game to MongoDB');
             res.redirect('/add');
           });
         });
@@ -296,9 +300,13 @@ app.post('/add', function (req, res) {
 });
 
 app.get('/', function(req, res) {
+
+  // users with less than 3 intersts or who still use temp. password should not be able to view this page
   if (req.session.tempPassword || req.session.user && req.session.user.interests.length < 3) {
     return res.redirect('/account');
   }
+
+  // If user is logged in then display 6 items based on interests and previous purchases
   if (req.session.user) {
     Game
       .find()
@@ -314,7 +322,9 @@ app.get('/', function(req, res) {
           customgames: games
         });
       });
-  } else {
+  }
+  // Visitors (guests) get 3 most popular game titles instead
+  else {
     Game
       .find()
       .limit(3)
