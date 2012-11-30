77a41ba3b3e054d64b01ade8facbf6ec8baa8695
Sahat Yalkabov <sakhat@gmail.com>
2012-11-30 02:07:07 -0500

    commented out redis support

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ app.configure(function () {
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.cookieParser('s3cr3t'));
-  //app.use(express.session({ secret: 's3cr3t' }));
-  app.use(express.session({ store: new RedisStore(), secret: 's3cr3t' }));
+  app.use(express.session({ secret: 's3cr3t' }));
+  //app.use(express.session({ store: new RedisStore(), secret: 's3cr3t' }));
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
